1b736cf8fac5f345e812e25d6fbc9ecdb6e8308f
pc cedtec <hugo_correa_g@hotmail.com>
2023-08-16 15:55:01 -0300

    Iniciado o projeto, adicionado jet pack compose, funçoes de saudaçao e myapp

--- app/src/main/java/com/github/hugocorrea700/jetpackcomposeinicio/MainActivity.kt
+++ app/src/main/java/com/github/hugocorrea700/jetpackcomposeinicio/MainActivity.kt
@@ -3,12 +3,13 @@ package com.github.hugocorrea700.jetpackcomposeinicio
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,22 +29,92 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colors.primary) {
-        Text(text = "BEM-VINDO $name", modifier = Modifier.padding(24.dp))
+    val expanded = remember { mutableStateOf(false)}
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
+
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier =
+            Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding)
+            ) {
+                Text(text = "BEM-VINDO, ")
+                Text(text = name)
+            }
+            Button(
+                onClick = { expanded.value = !expanded.value },
+            ) {
+                Text(if (expanded.value) "Esconder" else "Ver mais")
+            }
+        }
     }
 }
 
-@Preview(showBackground = true)
+
+@Composable
+fun MyApp(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("World", "Compose")
+) {
+    Column(modifier = modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
+    }
+}
+
+
+
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     JetpackcomposeinicioTheme {
-        MyApp()
+        MyApp(
+        )
     }
 }
 
 @Composable
-fun MyApp(modifier:Modifier=Modifier){
-    Surface(modifier=modifier, color=MaterialTheme.colors.background){
-        Greeting("Android")
+fun OnboardingScreen(
+    onContinueClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Bem-vindo ao Basics Codelab!")
+        Button(
+            modifier = Modifier
+                .padding(vertical = 24.dp),
+            onClick = onContinueClicked
+        ) {
+            Text("Continuar")
+        }
+    }
+
+}
+
+
+@Preview
+@Composable
+fun MyAppPreview() {
+    JetpackcomposeinicioTheme {
+        MyApp(Modifier.fillMaxSize())
+    }
+}
+
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    JetpackcomposeinicioTheme() {
+        OnboardingScreen(onContinueClicked = {})
     }
 }
